5edb08924c75da6d652f920e5dea6472af4492a9
ry-watanabe <ry-watanabe@sios.com>
2023-08-29 10:52:50 +0900

    .envファイルからトークンを取得

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+	tkn := os.Getenv("APIKEY")
+
 	// アクセストークンを使用してクライアントを生成する
-	tkn := "xoxb-5807191892403-5807304960274-aKfXEBIbtGDUX45j7adp4QSd"
 	api := slack.New(tkn)
-	group(api)
+	post(api)
 }
 
 func post(api *slack.Client) {
 	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
-	_, _, err := api.PostMessage("#public", slack.MsgOptionText("Hello World", true))
+	_, _, err := api.PostMessage("#general", slack.MsgOptionText("Hello World", true))
 	if err != nil {
 		panic(err)
 	}
