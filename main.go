20395c31616a617f0e5d479adc7250d333fa73f0
ry-watanabe <ry-watanabe@sios.com>
2023-08-31 11:13:12 +0900

    example.comをget

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	url := "http://example.com"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(byteArray))
+}
+
+func readEnv() string {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
-	//	tkn := os.Getenv("APIKEY")
-
+	tkn := os.Getenv("APIKEY")
+	return tkn
 }
