fc17996412a61821bed9b04443ab9b8d53618959
ry-watanabe <ry-watanabe@sios.com>
2023-08-31 11:12:42 +0900

    複雑なテキストに関するコードを削除

--- usingSlackLibrary.go
+++ usingSlackLibrary.go
@@ -25,49 +25,6 @@ func post(api *slack.Client) {
 	}
 }
 
-// 複雑なテキストを投稿する例
-func examplePost(api *slack.Client) {
-	attachment := slack.Attachment{
-		Pretext:    "This is slack post test by Go",
-		Title:      "title",
-		Color:      "#36a64f",
-		AuthorName: "author_name",
-		AuthorIcon: "https://placeimg.com/16/16/people",
-		MarkdownIn: []string{"`textTomarkdown`"},
-		Text:       "hello world `textTomarkdown`",
-		ThumbURL:   "http://placekitten.com/g/200/200",
-		FooterIcon: "https://platform.slack-edge.com/img/default_application_icon.png",
-
-		Fields: []slack.AttachmentField{
-			slack.AttachmentField{
-				Title: "Item1",
-				Value: "this is value of item1",
-				Short: false,
-			}, slack.AttachmentField{
-				Title: "Item2",
-				Value: "this is value of item2",
-				Short: true,
-			}, slack.AttachmentField{
-				Title: "Item3",
-				Value: "```" + "this is value of item3" + "```",
-				Short: false,
-			},
-		},
-	}
-
-	channelID, timestamp, err := api.PostMessage(
-		"#b",
-		slack.MsgOptionText("This is Title", false),
-		slack.MsgOptionAttachments(attachment),
-		slack.MsgOptionAsUser(true),
-	)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
-}
-
 // チャンネルに参加する
 func join(api *slack.Client, channelID string) {
 	_, _, _, err := api.JoinConversation(channelID)
